9afbe68adbbabfe9aeced0666960e99197de80de
Karim <unbelt@outlook.com>
2014-11-27 15:06:52 +0200

    Basic Parse.com API added

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -6,9 +6,48 @@ require.config({
 });
 
 require(['jquery'], function ($) {
-    if ($) {
-        console.log('Hello from jQuery!');
-    }
+    $(function () {
+        'use strict';
+
+        // target _blank to all external links
+        $('body a').each(function () {
+            if (this.href.indexOf(location.hostname) === -1) {
+                $(this).attr('target', '_blank');
+            }
+        });
+
+        // Setup Parse.com API
+        $.ajaxSetup({
+            headers: {
+                'X-Parse-Application-Id': 'TgePKQZ0V5ILd4oSi6iXce2x5e2hYnJdpb26am3a',
+                'X-Parse-REST-API-Key': 'nl4VVJVTrFoAE6ts5ooqGYQahRBc084EHc3IMGRg'
+            }
+        });
+
+        // Get Parse.com data
+        function getData(table) {
+            $.ajax({
+                method: 'GET',
+                url: 'https://api.parse.com/1/classes/' + table,
+                success: function (data) {
+                    $('#container').append(JSON.stringify(data.results) + '<br><br>');
+                },
+                error: function () {
+                    console.log('Opss...');
+                }
+            });
+        }
+
+        // All Categories
+        getData('Category');
+
+        // All Posts
+        getData('Post');
+
+        // All Users
+        getData('User');
+
+    }());
 });
 
 require(['underscore'], function (_) {
