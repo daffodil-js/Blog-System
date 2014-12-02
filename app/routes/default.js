f2e43ffb1b6a3859e491b50f79b8a767dcf7528d
Karim <unbelt@outlook.com>
2014-12-02 23:53:04 +0200

    initial pages added

--- app/routes/default.js
+++ app/routes/default.js
@@ -4,15 +4,17 @@ var express = require('express'),
 /* GET pages */
 router.get('*', function (req, res) {
     var page = req.originalUrl.split('/')[1],
+        title = page.charAt(0).toUpperCase() + page.slice(1),
         id = req.originalUrl.split('/')[2];
 
-    if (page === '') {
-        page = 'home'
+    if (page === '' || page === 'home') {
+        page = 'home';
+        title = 'Latest Posts';
     }
 
     res.render('default', {
         page: 'pages/' + page,
-        title: 'The Blob | ' + page.charAt(0).toUpperCase() + page.slice(1),
+        title: title,
         id: id
     });
 });
